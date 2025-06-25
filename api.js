0e2d4ff4b4774f3d77c83da62a544211a08d8482
Setaply <contact.setaply@gmail.com>
2025-06-25 18:14:52 +0200

    init

---
+++ api.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+const api = express();
+
+api.use(cors());
+api.use(express.json());
+
+api.get('/get-gps', async (req, res) => {
+  res.json((await fetch("http://217.154.87.99:3000/get-gps")).json());
+});
