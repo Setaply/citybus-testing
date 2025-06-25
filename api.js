1100d302b83c97ed4f76bacbd301a85f7412b208
Setaply <contact.setaply@gmail.com>
2025-06-25 18:17:24 +0200

    Update api.js

--- api.js
+++ api.js
@@ -8,3 +8,7 @@ api.use(express.json());
 api.get('/get-gps', async (req, res) => {
   res.json((await fetch("http://217.154.87.99:3000/get-gps")).json());
 });
+
+api.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
